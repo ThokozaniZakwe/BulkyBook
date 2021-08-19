36554d5426a28309e1f9f3e9178c46322cad050f
Thokozani Zakwe <zakwet@vodamail.co.za>
2021-08-19 08:31:52 +0200

    Add project files.

---
+++ BulkyBook.DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,35 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BulkyBook.DataAccess.Data;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class UnitOfWork: IUnitOfWork
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            Category = new CategoryRepository(db);
+            SP_Call = new SP_Call(db);
+        }
+
+        public ICategoryRepository Category { get; private set; }
+        public ISP_Call SP_Call { get; private set; }
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+
+        public async Task Save()
+        {
+            await _db.SaveChangesAsync();
+        }
+    }
+}
